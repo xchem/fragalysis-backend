0710aad64ccbaac77db58e5ca6ec0d4540f06eb6
abradle <anthony.richard.bradley@gmail.com>
2017-10-22 15:31:57 +0100

    Sort out Django errors

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python manage.py migrate        # Apply database migrations
-python manage.py collectstatic --clear --noinput # clearstatic files
-python manage.py collectstatic --noinput  # collect static files
+#python manage.py collectstatic --clear --noinput # clearstatic files
+#python manage.py collectstatic --noinput  # collect static files
 # Prepare log files and start outputting logs to stdout
 touch /srv/logs/gunicorn.log
 touch /srv/logs/access.log
