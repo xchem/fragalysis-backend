84815c1fb58aff176372b465cb78e40b827c365f
abradle <anthony.richard.bradley@gmail.com>
2017-10-28 11:26:51 +0200

    Fix URLs

--- network/urls.py
+++ network/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/query/$', views.query, name='query'),
-    url(r'^/display/$', views.display, name='display'),
+    url(r'^query/$', views.query, name='query'),
+    url(r'^display/$', views.display, name='display'),
 
 ]
