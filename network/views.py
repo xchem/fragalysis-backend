cece14c59860855255d80975b294b39f49304ef8
abradle <anthony.richard.bradley@gmail.com>
2017-10-28 11:07:42 +0200

    Updated the first view

--- network/views.py
+++ network/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
-from frag.network import
+import json
+from frag.network.query import get_picks
 
 def index(request):
-    if "smiles" in request.GET:
+    if "smiles" in request.GET \
+            and "num_picks" in request.GET:
         smiles = request.GET["smiles"]
-        return HttpResponse("Hello, world. You're at the polls index.")
+        num_picks = request.GET["smiles"]
+        out_dict = get_picks(smiles, num_picks)
+        return HttpResponse(json.dumps(out_dict))
     else:
         return HttpResponse("Please insert SMILES")
