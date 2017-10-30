d1c431bb1f6775715c800b62debaa0ffef6c202a
abradle <anthony.richard.bradley@gmail.com>
2017-10-30 17:36:01 +0000

    Improved login redirect

--- fragalysis/settings.py
+++ fragalysis/settings.py
@@ -75,7 +75,10 @@ AUTHENTICATION_BACKENDS = (
 )
 
 
+# CAS parameters
 CAS_SERVER_URL = "https://auth.diamond.ac.uk:443/cas/"
+CAS_REDIRECT_URL = "/network/display/"
+
 ROOT_URLCONF = 'fragalysis.urls'
 
 TEMPLATES = [
