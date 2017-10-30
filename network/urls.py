cb135ae4b0f9399ca8cd3fed60210e2499b88ed7
abradle <anthony.richard.bradley@gmail.com>
2017-10-30 17:41:11 +0000

    Split out the views

--- network/urls.py
+++ network/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^query/$', views.query, name='query'),
+    url(r'^query/$', views.full_graph, name='full_graph'),
+    url(r'^query/$', views.pick_mols, name='pick_mols'),
     url(r'^display/$', views.display, name='display'),
 
 ]
--- network/views.py
+++ network/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 import json
-from frag.network.query import get_picks
+from frag.network.query import get_picks,get_full_graph
 from django.shortcuts import render
 
 
-def query(request):
+def pick_mols(request):
     if "smiles" in request.GET \
             and "num_picks" in request.GET:
         smiles = request.GET["smiles"]
@@ -14,6 +14,14 @@ def query(request):
     else:
         return HttpResponse("Please insert SMILES")
 
+def full_graph(request):
+    if "smiles" in request.GET \
+            and "num_picks" in request.GET:
+        smiles = request.GET["smiles"]
+        out_dict = get_full_graph(smiles)
+        return HttpResponse(json.dumps(out_dict))
+    else:
+        return HttpResponse("Please insert SMILES")
 
 def display(request):
     return render(request, 'network/display.html', {})
