efa3d3a2fdf0a35943ee197385e9359d5cc258fe
abradle <anthony.richard.bradley@gmail.com>
2017-12-09 12:11:46 +0000

    Remove cas login

--- fragalysis/urls.py
+++ fragalysis/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     url(r'^accounts/login$', django_cas_ng.views.login, name='cas_ng_login'),
     url(r'^accounts/logout$', django_cas_ng.views.logout, name='cas_ng_logout'),
     url(r'^accounts/callback$', django_cas_ng.views.callback, name='cas_ng_proxy_callback'),
-    url(r'^$', RedirectView.as_view(url='/accounts/login'),
+    url(r'^$', RedirectView.as_view(url='/network/display/'),
+    #url(r'^$', RedirectView.as_view(url='/accounts/login'),
 )
 ]
