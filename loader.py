deb03d2655840e88174dd8d33d2860b69f2d53b8
abradle <anthony.richard.bradley@gmail.com>
2018-02-02 16:12:28 +0100

    Quick fix

--- loader.py
+++ loader.py
@@ -1,4 +1,7 @@
-from viewer.loaders import load_from_dir
-
+import os
 if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fragalysis.settings")
+    import django
+    django.setup()
+    from viewer.loaders import load_from_dir
     load_from_dir("DCP2B","/code/media/DCP2B")
