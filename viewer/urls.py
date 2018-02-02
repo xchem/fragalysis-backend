f22da8786ffe002fa5e756c746db767e627317d7
abradle <anthony.richard.bradley@gmail.com>
2018-02-02 16:47:54 +0100

    Quick fix

--- viewer/urls.py
+++ viewer/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url,path
+from django.conf.urls import url
 
 from . import views
 
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^display/$', views.display, name='display'),
     url(r'^mol_view/$', views.mol_view, name='mol_view'),
     url(r'^img_from_pk/$', views.img_from_pk, name='img_from_pk'),
-    path('/viewer/prot_from_pk/<int:pk>/ ', views.prot_from_pk, name='prot_from_pk'),
-    path('/viewer/mol_from_pk/<int:pk>/', views.mol_from_pk, name='mol_from_pk'),
+    url(r'^prot_from_pk/(?P<pk>[0-9]+)/$', views.prot_from_pk, name='prot_from_pk'),
+    url(r'^mol_from_pk/(?P<pk>[0-9]+)/', views.mol_from_pk, name='mol_from_pk'),
     url(r'^post_view/$', views.post_view, name='post_view'),
 ]
