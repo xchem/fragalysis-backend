c6455c81dd78e6ed67f4cc6e6e7fd23db097f78c
abradle <anthony.richard.bradley@gmail.com>
2018-02-27 16:57:49 +0000

    Updated to use routers correctly

--- api/urls.py
+++ api/urls.py
@@ -3,15 +3,14 @@ from rest_framework.authtoken import views as drf_views
 from rest_framework.routers import DefaultRouter
 from api import views
 
+router = DefaultRouter()
+router.register(r'molecules', views.MoleculeView)
+router.register(r'compounds', views.CompoundView)
+router.register(r'targets', views.TargetView)
+router.register(r'proteins', views.ProteinView)
+
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^auth$', drf_views.obtain_auth_token, name='auth'),
-    url(r'^molecules/$', views.MoleculeList.as_view()),
-    url(r'^compounds/$', views.CompoundList.as_view()),
-    url(r'^proteins/$', views.ProteinList.as_view()),
-    url(r'^target/$', views.TargetList.as_view()),
-    url(r'^compounds/(?P<pk>[0-9]+)/$', views.CompoundDetail.as_view()),
-    url(r'^molecules/(?P<pk>[0-9]+)/$', views.MoleculeDetail.as_view()),
-    url(r'^proteins/(?P<pk>[0-9]+)/$', views.ProteinDetail.as_view()),
-    url(r'^targets/(?P<pk>[0-9]+)/$', views.TargetDetail.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -3,49 +3,28 @@ from viewer.serializers import ViewSceneSerializer,ActivityPointSerializer,Molec
     ProteinSerializer, CompoundSerializer, TargetSerializer
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import viewsets
 
-class TargetList(generics.ListAPIView):
+class TargetView(generics.ListAPIView,
+                 viewsets.ModelViewSet):
     queryset = Target.objects.filter()
     serializer_class = TargetSerializer
     # permission_classes =  [permissions.DjangoObjectPermissions(),]
     filter_fields = ('title',)
 
-
-class TargetDetail(generics.RetrieveAPIView):
-    queryset = Target.objects.filter()
-    serializer_class = TargetSerializer
-    # permission_classes =  [permissions.DjangoObjectPermissions(),]
-    filter_fields = ('title',)
-
-
-class MoleculeList(generics.ListAPIView):
+class MoleculeView(generics.ListAPIView):
     queryset = Molecule.objects.filter()
     serializer_class = MoleculeSerializer
     filter_fields = ('prot_id', 'cmpd_id','smiles')
 
-class MoleculeDetail(generics.RetrieveAPIView):
-    queryset = Molecule.objects.filter()
-    serializer_class = MoleculeSerializer
-    filter_fields = ('prot_id', 'cmpd_id','smiles')
-
-
-class CompoundList(generics.ListAPIView):
+class CompoundView(generics.ListAPIView,
+                   viewsets.ModelViewSet):
     queryset = Compound.objects.filter()
     serializer_class = CompoundSerializer
     filter_fields = ('smiles',)
 
-class CompoundDetail(generics.RetrieveAPIView):
-    queryset = Compound.objects.filter()
-    serializer_class = CompoundSerializer
-    filter_fields = ('smiles',)
-
-
-class ProteinList(generics.ListAPIView):
-    queryset = Protein.objects.filter()
-    serializer_class = ProteinSerializer
-    filter_fields = ('code','target_id',)
-
-class ProteinDetail(generics.RetrieveAPIView):
+class ProteinView(generics.ListAPIView,
+                  viewsets.ModelViewSet):
     queryset = Protein.objects.filter()
     serializer_class = ProteinSerializer
     filter_fields = ('code','target_id',)
