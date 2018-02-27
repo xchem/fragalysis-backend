8399bd82c197ee308053321552ba8d7ad3549358
abradle <anthony.richard.bradley@gmail.com>
2018-02-27 17:01:48 +0000

    Removed generic view

--- api/views.py
+++ api/views.py
@@ -5,26 +5,23 @@ from rest_framework import generics
 from rest_framework import permissions
 from rest_framework import viewsets
 
-class TargetView(generics.ListAPIView,
-                 viewsets.ModelViewSet):
+class TargetView(viewsets.ModelViewSet):
     queryset = Target.objects.filter()
     serializer_class = TargetSerializer
     # permission_classes =  [permissions.DjangoObjectPermissions(),]
     filter_fields = ('title',)
 
-class MoleculeView(generics.ListAPIView):
+class MoleculeView(viewsets.ModelViewSet):
     queryset = Molecule.objects.filter()
     serializer_class = MoleculeSerializer
     filter_fields = ('prot_id', 'cmpd_id','smiles')
 
-class CompoundView(generics.ListAPIView,
-                   viewsets.ModelViewSet):
+class CompoundView(viewsets.ModelViewSet):
     queryset = Compound.objects.filter()
     serializer_class = CompoundSerializer
     filter_fields = ('smiles',)
 
-class ProteinView(generics.ListAPIView,
-                  viewsets.ModelViewSet):
+class ProteinView(viewsets.ModelViewSet):
     queryset = Protein.objects.filter()
     serializer_class = ProteinSerializer
     filter_fields = ('code','target_id',)
