78e5de944507ee4e45823c54cc33e2bed9820b4b
abradle <anthony.richard.bradley@gmail.com>
2018-02-27 17:46:04 +0000

    Fixed for loader

--- loader.py
+++ loader.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     targets_to_load = ["CAMK1DA","DCLRE1AA","DCP2B","FALZA","MURD","NUDT21A","NUDT22A","NUDT7A",
                        "PARP14A","PHIPA","SETDB1","SHMT2A","HAO1A"]
     for t in targets_to_load:
-        load_from_dir(t,"/code/media/"+t)
+        load_from_dir(t,"/media/"+t)
