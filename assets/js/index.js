2caa30dcac71e149eb372788de8c6dbedd3f6608
abradle <anthony.richard.bradley@gmail.com>
2018-03-01 14:13:01 +0000

    DEBUG

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,7 +16,8 @@ class TotalView extends React.Component {
         this.onTargetChecked = this.onTargetChecked.bind(this)
         this.onMolChecked = this.onMolChecked.bind(this)
         this.mol_list = new Array();
-        this.state.mol_params = {"target_id": 1}
+        this.state = {mol_params: {"target_id": 1}
+        }
     }
     
     onTargetChecked(target){
