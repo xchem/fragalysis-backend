08d27418fa4a1a843ae252a9628a3bd82750e669
abradle <anthony.richard.bradley@gmail.com>
2018-03-02 05:56:59 +0000

    DEBUG

--- assets/js/components/ngl_components.js
+++ assets/js/components/ngl_components.js
@@ -18,13 +18,29 @@ export class NGLView extends React.Component {
         this.old_dict = {}
         this.mol_url = "/viewer/mol_from_pk/"
         this.prot_url = "/viewer/prot_from_pk/"
-        this.show_mol = this.show_mol.bind(this);
+        this.showMol = this.showMol.bind(this);
         this.removeComponentByName = this.removeComponentByName.bind(this);
         this.molLigInteraction = this.molLigInteraction.bind(this);
+        this.genMolLigInt = this.genMolLigInt.bind(this);
+        this.getInputDict = this.getInputDict.bind(this);
     }
 
-    removeComponentByName(compName){
+
+    componentDidMount(){
+        this.stage = new Stage(this.div_id);
+        // Handle window resizing
+        window.addEventListener("resize", function (event) {
+            this.stage.handleResize();
+        }, false);
+        this.showMol();
+        setInterval(this.showMol,
+            this.interval)
+    }
+
+
+    removeComponentByName(inputDict){
         var local_stage = this.stage
+        var compName = inputDict["object_name"]
         local_stage.eachComponent(
             function(comp){
                 if (comp.name==compName){
@@ -33,66 +49,63 @@ export class NGLView extends React.Component {
             })
     }
 
-    componentDidMount(){
-        this.stage = new Stage(this.div_id);
-        // Handle window resizing
-        window.addEventListener("resize", function (event) {
-            this.stage.handleResize();
-        }, false);
-        this.show_mol();
-        setInterval(this.show_mol,
-            this.interval)
-    }
-    molLigInteraction(prot_url,mol_url,object_name){
+    molLigInteraction(input_dict){
         Promise.all([
-                this.stage.loadFile(prot_url, {ext: "pdb"}),
-                this.stage.loadFile(mol_url, {ext: "sdf"}),
-                this.stage, this.focus_var, object_name]
-            ).then(function (ol) {
-                var cs = concatStructures(
-                    ol[4],
-                    ol[0].structure.getView(new Selection("not ligand")),
-                    ol[1].structure.getView(new Selection(""))
-                )
-                var stage = ol[2];
-                var focus_var = ol[3];
-                // Set the object name
-                var comp = stage.addComponentFromObject(cs)
-                comp.addRepresentation("cartoon")
-                comp.addRepresentation("contact", {
-                    masterModelIndex: 0,
-                    weakHydrogenBond: true,
-                    maxHbondDonPlaneAngle: 35,
-                    sele: "/0 or /1"
-                })
-                comp.addRepresentation("licorice", {
-                    sele: "ligand and /1",
-                    multipleBond: "offset"
-                })
-                comp.addRepresentation("line", {
-                    sele: "/0"
-                })
-                comp.autoView("ligand");
-                stage.setFocus(focus_var);
-                NProgress.done();
-        });
+                this.stage.loadFile(input_dict["prot_url"], {ext: "pdb"}),
+                this.stage.loadFile(input_dict["mol_url"], {ext: "sdf"}),
+                this.stage, this.focus_var, input_dict["object_name"]]
+            ).then(genMolLigInt(ol));
     }
 
+    genMolLigInt(ol){
+            var cs = concatStructures(
+                ol[4],
+                ol[0].structure.getView(new Selection("not ligand")),
+                ol[1].structure.getView(new Selection(""))
+            )
+            var stage = ol[2];
+            var focus_var = ol[3];
+            // Set the object name
+            var comp = stage.addComponentFromObject(cs)
+            comp.addRepresentation("cartoon")
+            comp.addRepresentation("contact", {
+                masterModelIndex: 0,
+                weakHydrogenBond: true,
+                maxHbondDonPlaneAngle: 35,
+                sele: "/0 or /1"
+            })
+            comp.addRepresentation("licorice", {
+                sele: "ligand and /1",
+                multipleBond: "offset"
+            })
+            comp.addRepresentation("line", {
+                sele: "/0"
+            })
+            comp.autoView("ligand");
+            stage.setFocus(focus_var);
+            NProgress.done();
+    }
 
-    show_mol() {
+    getInputDict(mol_dict){
+        var prot_id = this.props.mol_dict["prot_id"]
+        var mol_id = this.props.mol_dict["mol_id"]
+        var toggle = this.props.mol_dict["toggle"]
+        var prot_url = this.prot_url + prot_id.toString() + "/"
+        var mol_url = this.mol_url + mol_id.toString() + "/"
+        var object_name = mol_id.toString()+"_mol"
+        var inputDict ={"mol_url": mol_url, "prot_url": prot_url, "object_name":object_name, "toggle": toggle}
+        return inputDict
+    }
+
+    showMol() {
         if (this.props.mol_dict && this.props.mol_dict != this.old_dict) {
-            const prot_id = this.props.mol_dict["prot_id"]
-            const mol_id = this.props.mol_dict["mol_id"]
-            const toggle = this.props.mol_dict["toggle"]
-            const prot_url = this.prot_url + prot_id.toString() + "/"
-            const mol_url = this.mol_url + mol_id.toString() + "/"
-            const object_name = mol_id.toString()+"_mol"
+            var inputDict = getInputDict(this.props.mol_dict);
             NProgress.start();
-            if(toggle==true) {
-                this.molLigInteraction(prot_url,mol_url,object_name)
+            if(inputDict["toggle"]==true) {
+                this.molLigInteraction(inputDict)
             }
             else{
-                this.removeComponentByName(object_name);
+                this.removeComponentByName(inputDict);
                 NProgress.done();
             }
         }
