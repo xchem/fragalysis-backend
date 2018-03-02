f464ef47737c1bb25130c66016f21ecf79214d8f
abradle <anthony.richard.bradley@gmail.com>
2018-03-02 08:59:08 +0000

    Added scoring models

--- viewer/models.py
+++ viewer/models.py
@@ -129,6 +129,43 @@ class ActivityPoint(models.Model):
             ('view_activitypoint', 'View activitypoint'),
         )
 
+
+# TODO - Refactor this out to a different APP
+class MolGroup(models.Model):
+    """
+    A Django model for a group of molecules.
+    No unique set - so needs to be deleted for a type before re-running.
+    """
+    PANDDA = 'PA'
+    DEFAULT = 'DE'
+    MOL_CLUSTER = 'MC'
+    WATER_CLUSTER = 'WC'
+    PHARMA_CLUSTER = 'PC'
+    RES_CLUSTER = "RC"
+    MOL_GROUP_CHOICES = (
+        (PANDDA, 'Pandda'),
+        (DEFAULT, 'Default'),
+        (MOL_CLUSTER, 'MolCluster'),
+        (WATER_CLUSTER, 'WaterCluster'),
+        (PHARMA_CLUSTER, 'PharmaCluster'),
+        (RES_CLUSTER, 'ResCluster'),
+    )
+    # Set the groups types
+    group_type = models.CharField(choices=MOL_GROUP_CHOICES,max_length=2,default=DEFAULT)
+    # Set the target id
+    target_id = models.ForeignKey(Target)
+    # Set the ManyToMany
+    mol_id = models.ManyToManyRel(Molecule)
+    # Set the centre of mass
+    x_com = models.FloatField(null=True)
+    y_com = models.FloatField(null=True)
+    z_com = models.FloatField(null=True)
+
+    class Meta:
+        permissions = (
+            ('view_molgroup', 'View molecule group'),
+        )
+
 class ViewScene(models.Model):
     """
     A Django Model for a given view.
@@ -151,12 +188,17 @@ class ProtChoice(models.Model):
     """
     user_id = models.ForeignKey(User)
     prot_id = models.ForeignKey(Protein)
-    text = models.TextField(default="DEFAULT")
-    # Score between 0 and 9; Convention for n memberd list -> num_in_list/(num_choices-1)
-    # E.g.
-    score = models.IntegerField()
+    # Set the groups types
+    DEFAULT = 'DE'
+    PROT_CHOICES = (
+        (DEFAULT, 'Default'),
+    )
+    choice_type = models.CharField(choices=PROT_CHOICES,max_length=2,default=DEFAULT)
+    # Integer Score for this
+    score = models.FloatField(null=True)
+
     class Meta:
-        unique_together = ('user_id', 'prot_id', 'text',)
+        unique_together = ('user_id', 'prot_id', 'choice_type',)
         permissions = (
             ('view_protchoice', 'View protchoice'),
         )
@@ -168,16 +210,53 @@ class MolChoice(models.Model):
     """
     user_id = models.ForeignKey(User)
     mol_id = models.ForeignKey(Molecule)
-    text = models.TextField(default="DENSITY")
-    # Score between 0 and 9; Convention for n memberd list -> num_in_list/(num_choices-1)
-    # E.g.
-    score = models.IntegerField(null=True)
+    DEFAULT = 'DE'
+    PANDDA = 'PA'
+    GOOD_MOL = 'GM'
+    MOL_CHOICES = (
+        (DEFAULT, 'Default'),
+        (PANDDA, 'Pandda'),
+        (GOOD_MOL, 'Good molecule'),
+    )
+    choice_type = models.CharField(choices=MOL_CHOICES,max_length=2,default=DEFAULT)
+    # Score -
+    score = models.FloatField(null=True)
+
     class Meta:
-        unique_together = ('user_id', 'mol_id', 'text')
+        unique_together = ('user_id', 'mol_id', 'choice_type')
         permissions = (
             ('view_molchoice', 'View molchoice'),
         )
 
+
+class ScoreChoice(models.Model):
+    """
+    A Django model to store a selection from a user
+    """
+    # IN THIS CASE THIS WOULD INDICATE THE SOFTWARE USED - WE WILL GENERATE DIFFERENT USERS FOR EACH SOFTWARE
+    user_id = models.ForeignKey(User)
+    mol_id = models.ForeignKey(Molecule)
+    prot_id = models.ForeignKey(Molecule)
+    is_done = models.BooleanField(default=False)
+    DEFAULT = 'DE'
+    DOCKING = 'AU'
+    DENSITY_FIT = 'DF'
+    INTERACTION = "IT"
+    DOCK_CHOICES = (
+        (DEFAULT, 'Default'),
+        (DOCKING, 'Docking'),
+        (INTERACTION, 'Interaction fit'),
+        (DENSITY_FIT, 'Density Fit'),
+    )
+    choice_type = models.CharField(choices=DOCK_CHOICES,max_length=2,default=DEFAULT)
+    # Any score
+    score = models.FloatField(null=True)
+
+    class Meta:
+        unique_together = ('user_id', 'mol_id', 'prot_id', 'choice_type')
+        permissions = (
+            ('view_scorechoice', 'View scorechoice'),
+        )
 
 class CmpdChoice(models.Model):
     """
@@ -185,12 +264,21 @@ class CmpdChoice(models.Model):
     """
     user_id = models.ForeignKey(User)
     cmpd_id = models.ForeignKey(Compound)
-    text = models.TextField(default="DEFAULT")
+    DEFAULT = 'DE'
+    PRICE = 'PR'
+    TOXIC = 'TO'
+    CMPD_CHOICES = (
+        (DEFAULT, 'Default'),
+        (PRICE, 'Price'),
+        (TOXIC, 'Toxic'),
+    )
+    choice_type = models.CharField(choices=CMPD_CHOICES,max_length=2,default=DEFAULT)
     # Score between 0 and 9; Convention for n memberd list -> num_in_list/(num_choices-1)
     # E.g.
-    score = models.IntegerField()
+    score = models.FloatField(null=True)
+
     class Meta:
-        unique_together = ('user_id', 'cmpd_id','text',)
+        unique_together = ('user_id', 'cmpd_id','choice_type',)
         permissions = (
             ('view_cmpdchoice', 'View cmpdhoice'),
         )
