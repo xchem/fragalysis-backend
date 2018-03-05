74ecd9b274910b9b055255c789f00c974002a676
abradle <anthony.richard.bradley@gmail.com>
2018-03-05 13:46:06 +0000

    DEBUG

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -29,6 +29,8 @@ class LoaderFunctionsTestCase(TestCase):
 class LoderLoaderTestCase(TestCase):
 
     def setUp(self):
+        out_f = open('DUMMY_NEW.pdb', "w")
+        out_f = open("DUMMY_NEW_TWO.pdb", "w")
 
         self.mol_one = Chem.MolFromSmiles("CCC[NH+]CCC.Cl")
         self.mol_two = Chem.MolFromSmiles("CCC[NH+]CCC")
@@ -40,7 +42,7 @@ class LoderLoaderTestCase(TestCase):
         self.mol_sd_two_str = "\n     RDKit          3D\n\n 18 19  0  0  0  0  0  0  0  0999 V2000\n  -34.3680   23.4240   76.0460 C   0  0  0  0  0  0  0  0  0  0  0  0\n  -40.7600   21.3970   82.8690 C   0  0  0  0  0  0  0  0  0  0  0  0\n  -40.3490   20.2370   83.4190 C   0  0  0  0  0  0  0  0  0  0  0  0\n  -40.1630   19.0970   82.7260 C   0  0  0  0  0  0  0  0  0  0  0  0\n  -40.3690   19.1280   81.3680 C   0  0  0  0  0  0  0  0  0  0  0  0\n  -35.7060   24.0440   75.8520 C   0  0  0  0  0  0  0  0  0  0  0  0\n  -36.8250   22.2590   77.2910 C   0  0  0  0  0  0  0  0  0  0  0  0\n  -38.0350   22.2270   77.9400 C   0  0  0  0  0  0  0  0  0  0  0  0\n  -38.6470   23.4280   77.5950 C   0  0  0  0  0  0  0  0  0  0  0  0\n  -38.7950   21.1690   78.6150 C   0  0  0  0  0  0  0  0  0  0  0  0\n  -41.0140   20.3310   79.2620 C   0  0  0  0  0  0  0  0  0  0  0  0\n  -40.8030   20.2790   80.7470 C   0  0  0  0  0  0  0  0  0  0  0  0\n  -40.9790   21.4150   81.5160 C   0  0  0  0  0  0  0  0  0  0  0  0\n  -40.1360   20.2240   84.7490 F   0  0  0  0  0  0  0  0  0  0  0  0\n  -36.7580   23.4430   76.6640 N   0  0  0  0  0  0  0  0  0  0  0  0\n  -37.8780   24.1510   76.8320 N   0  0  0  0  0  0  0  0  0  0  0  0\n  -40.1350   21.3160   78.6610 N   0  0  0  0  0  0  0  0  0  0  0  0\n  -38.2430   20.1280   78.9310 O   0  0  0  0  0  0  0  0  0  0  0  0\n  1  6  1  0\n  2  3  2  0\n  2 13  1  0\n  3  4  1  0\n  3 14  1  0\n  4  5  2  0\n  5 12  1  0\n  6 15  1  0\n  7  8  2  0\n  7 15  1  0\n  8  9  1  0\n  8 10  1  0\n  9 16  2  0\n 10 17  1  0\n 10 18  2  0\n 11 12  1  0\n 11 17  1  0\n 12 13  2  0\n 15 16  1  0\nM  END\n"
         out_f = open("mol.sd_one_two", "w")
         out_f.write(self.mol_sd_two_str)
-        self.mol_sd = out_f.name
+        self.mol_sd_two = out_f.name
         self.mol_smi =  "O=C(c1ccc2c(c1)OCO2)N1CCCCCC1"
         self.mol_smi_two = "Cn1cc(C(=O)NCc2ccc(F)cc2)cn1"
         # Build the dummy database file
@@ -98,17 +100,17 @@ class LoderLoaderTestCase(TestCase):
 
     def test_analyse_mols(self):
         analyse_mols(Molecule.objects.filter(prot_id__target_id=self.target),self.target)
-        self.assertEqual(MolGroup.objects.filter(group_type='MC').count(),2)
+        self.assertEqual(MolGroup.objects.filter(group_type='MC').count(),1)
         self.assertEqual(MolGroup.objects.filter(group_type='PC').count(),2)
         analyse_mols(Molecule.objects.filter(prot_id__target_id=self.target),self.target)
-        self.assertEqual(MolGroup.objects.filter(group_type='MC').count(), 2)
+        self.assertEqual(MolGroup.objects.filter(group_type='MC').count(), 1)
         self.assertEqual(MolGroup.objects.filter(group_type='PC').count(), 2)
 
     def test_anaylse_target(self):
         analyse_target("DUMMY_TARGET")
-        self.assertEqual(MolGroup.objects.filter(group_type='MC').count(),2)
+        self.assertEqual(MolGroup.objects.filter(group_type='MC').count(),1)
         self.assertEqual(MolGroup.objects.filter(group_type='PC').count(),2)
         analyse_target("DUMMY")
         analyse_target("DUMMY_TARGET")
-        self.assertEqual(MolGroup.objects.filter(group_type='MC').count(),2)
+        self.assertEqual(MolGroup.objects.filter(group_type='MC').count(),1)
         self.assertEqual(MolGroup.objects.filter(group_type='PC').count(),2)
