5e7f581912e9798b3312e723ed3d62ff030a1751
abradle <anthony.richard.bradley@gmail.com>
2018-03-05 11:36:42 +0000

    DEBUG

--- tests/test_api.py
+++ tests/test_api.py
@@ -69,7 +69,7 @@ class APIUrlsTestCase(TestCase):
                          {'id':1, 'inchi':"DUM_INCH", 'smiles':"DUM_SMI", 'mol_log_p':0.1, 'mol_wt':0.2,
                           'num_h_acceptors':5, 'num_h_donors':6},
                          {'id':1, 'title':"DUMMY_TARGET", 'project_id': []},
-                         {'id':1, 'code':"DUMM", 'target_id':1, 'pdb_info':"my_pdb.pdb", 'mtz_info':None,
+                         {'id':1, 'code':"DUMM", 'target_id':1, 'pdb_info':"http://testserver/media/my_pdb.pdb", 'mtz_info':None,
                           'map_info':None, 'cif_info':None}]
         for i,url in enumerate(urls):
             response = self.client.get(url_base+"/"+url+"/1/")
