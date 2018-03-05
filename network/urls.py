7360408d576e62f843a2d7bc9ad95d7ed866ad1d
abradle <anthony.richard.bradley@gmail.com>
2018-03-05 09:29:05 +0000

    Update

--- network/urls.py
+++ network/urls.py
@@ -4,8 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^full_graph/$', views.full_graph, name='full_graph'),
-    url(r'^pick_mols/$', views.pick_mols, name='pick_mols'),
     url(r'^display/$', views.display, name='display'),
-    url(r'^query_db/$', views.query_db, name='query_db'),
-
 ]
