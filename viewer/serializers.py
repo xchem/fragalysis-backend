31ec642c89989f595a5102a2c75f0bd3a4be0afe
abradle <you@example.com>
2018-04-18 18:03:42 +0100

    Bugfix

--- viewer/serializers.py
+++ viewer/serializers.py
@@ -37,7 +37,7 @@ class PanddaEventSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PanddaEvent
-        fields = ('xtal','event','pandda_site','target_id','pdb_info','mtz_info','map_info','lig_id',
+        fields = ('id','xtal','event','pandda_site','target_id','pdb_info','mtz_info','map_info','lig_id',
                   'event_com_x','event_com_y','event_com_z','lig_com_x','lig_com_y','lig_com_z',
                   'event_dist_from_site_centroid','lig_dist_from_site_centroid','small_map_info')
 
@@ -45,5 +45,5 @@ class PanddaSiteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PanddaSite
-        fields = ('site_id','pandda_run','pandda_version','target_id','site_align_com_x','site_align_com_y','site_align_com_z',
+        fields = ('id','site_id','pandda_run','pandda_version','target_id','site_align_com_x','site_align_com_y','site_align_com_z',
                   'site_native_com_x','site_native_com_y','site_native_com_z',)
