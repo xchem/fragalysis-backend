4eaeb06d57c7f90a84fb6d0f889b2eb325a42e81
abradle <you@example.com>
2018-05-17 18:06:16 +0100

    Simplify migrations

--- makemigrations.sh
+++ makemigrations.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 echo "Running migrations..."
 cd /code
-python /code/manage.py makemigrations auth
-python /code/manage.py migrate
-python /code/manage.py makemigrations scoring pandda hypothesis hotspots
-python /code/manage.py migrate
 python /code/manage.py makemigrations
 python /code/manage.py migrate  # Apply database migrations - weird order is due to https://stackoverflow.com/questions/31417470/django-db-utils-programmingerror-relation-app-user-does-not-exist-during-ma
 echo "Running loader..."
