a0188fa2938962e9d15c03f823d5c45f5c33e0ab
abradle <you@example.com>
2018-06-07 10:34:57 +0100

    Added basic celery implementation and updated timeout

--- viewer/views.py
+++ viewer/views.py
@@ -1,50 +1,60 @@
-from viewer.models import Molecule, Protein, Compound,Target
-from viewer.serializers import MoleculeSerializer, ProteinSerializer, CompoundSerializer, TargetSerializer
+from viewer.models import Molecule, Protein, Compound, Target
+from viewer.serializers import (
+    MoleculeSerializer,
+    ProteinSerializer,
+    CompoundSerializer,
+    TargetSerializer,
+)
 from rest_framework import permissions
 from rest_framework import viewsets
+
 
 class TargetView(viewsets.ReadOnlyModelViewSet):
     queryset = Target.objects.filter()
     serializer_class = TargetSerializer
     # permission_classes =  [permissions.DjangoObjectPermissions,]
-    filter_fields = ('title',)
+    filter_fields = ("title",)
+
 
 class MoleculeView(viewsets.ReadOnlyModelViewSet):
     queryset = Molecule.objects.filter()
     serializer_class = MoleculeSerializer
-    filter_fields = ('prot_id', 'cmpd_id','smiles','prot_id__target_id', 'mol_groups')
+    filter_fields = ("prot_id", "cmpd_id", "smiles", "prot_id__target_id", "mol_groups")
+
 
 class CompoundView(viewsets.ReadOnlyModelViewSet):
     queryset = Compound.objects.filter()
     serializer_class = CompoundSerializer
-    filter_fields = ('smiles',)
+    filter_fields = ("smiles",)
+
 
 class ProteinView(viewsets.ReadOnlyModelViewSet):
     queryset = Protein.objects.filter()
     serializer_class = ProteinSerializer
-    filter_fields = ('code','target_id',)
+    filter_fields = ("code", "target_id")
 
 
 from django.http import HttpResponse
 from django.shortcuts import render
-from viewer.models import Molecule,Protein,Compound
-from scoring.models import MolChoice,ViewScene
+from viewer.models import Molecule, Protein, Compound
+from scoring.models import MolChoice, ViewScene
 from uuid import uuid4
 import json
 from frag.network.decorate import get_3d_vects_for_mol
-from frag.network.query import get_full_graph
-from api.utils import draw_mol,get_token
+from api.utils import draw_mol, get_token
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .tasks import get_my_graph
+
+
 def react(request):
     """
     :param request:
     :return:
     """
-    return render(request,'viewer/react_temp.html',)
+    return render(request, "viewer/react_temp.html")
 
 
-
-def get_params(smiles,request):
+def get_params(smiles, request):
     height = None
     if "height" in request.GET:
         height = int(request.GET["height"])
@@ -53,12 +63,14 @@ def get_params(smiles,request):
         width = int(request.GET["width"])
     return HttpResponse(draw_mol(smiles, width=width, height=height))
 
+
 def mol_view(request):
     if "smiles" in request.GET:
         smiles = request.GET["smiles"].rstrip(".svg")
-        return get_params(smiles,request)
+        return get_params(smiles, request)
     else:
         return HttpResponse("Please insert SMILES")
+
 
 def img_from_pk(request):
     """
@@ -68,17 +80,20 @@ def img_from_pk(request):
     """
     if "pk" in request.GET:
         smiles = Molecule.objects.get(pk=request.GET["pk"]).smiles
-        return get_params(smiles,request)
+        return get_params(smiles, request)
     else:
         return HttpResponse("Please insert PK")
 
-def img_from_mol_pk(request,pk):
+
+def img_from_mol_pk(request, pk):
     smiles = Molecule.objects.get(pk=pk).smiles
     return get_params(smiles, request)
 
-def img_from_cmpd_pk(request,pk):
+
+def img_from_cmpd_pk(request, pk):
     smiles = Compound.objects.get(pk=pk).smiles
     return get_params(smiles, request)
+
 
 def img_from_smiles(request):
     if "smiles" in request.GET:
@@ -87,18 +102,20 @@ def img_from_smiles(request):
     else:
         return HttpResponse("Please insert SMILES")
 
+
 def mol_from_pk(request, pk):
     sdf_info = Molecule.objects.get(pk=pk).sdf_info
-    return  HttpResponse(sdf_info)
+    return HttpResponse(sdf_info)
 
 
-def map_from_pk(request,pk):
+def map_from_pk(request, pk):
     prot = Protein.objects.get(pk=pk)
     return HttpResponse(prot.map_info)
 
-def prot_from_pk(request,pk):
+
+def prot_from_pk(request, pk):
     pdb_info = open(Protein.objects.get(pk=pk).pdb_info.path).read()
-    return  HttpResponse(pdb_info)
+    return HttpResponse(pdb_info)
 
 
 def post_view(request):
@@ -112,8 +129,11 @@ def post_view(request):
     new_view.scene = request.POST["scene"]
     new_view.uuid = str(uuid4())
     new_view.save()
-    url = request.build_absolute_uri("?scene_id="+str(new_view.pk)).replace("/post_view/","/display/")
+    url = request.build_absolute_uri("?scene_id=" + str(new_view.pk)).replace(
+        "/post_view/", "/display/"
+    )
     return HttpResponse(url)
+
 
 def get_view(request, pk):
     """
@@ -122,16 +142,18 @@ def get_view(request, pk):
     :return:
     """
     this_view = ViewScene.objects.get(pk=pk)
-    return HttpResponse(json.dumps({"title": this_view.title,
-                         "scene": this_view.scene}))
+    return HttpResponse(
+        json.dumps({"title": this_view.title, "scene": this_view.scene})
+    )
+
 
 def get_vects_from_pk(request, pk):
     sdf_info = str(Molecule.objects.get(pk=pk).sdf_info)
     out_data = get_3d_vects_for_mol(sdf_info)
     return HttpResponse(json.dumps(out_data))
 
-def get_graph_from_pk(request,pk):
-    smiles = str(Molecule.objects.get(pk=pk).smiles)
-    out_data = get_full_graph(smiles)
-    return HttpResponse(json.dumps(out_data))
 
+def get_graph_from_pk(request, pk):
+    smiles = str(Molecule.objects.get(pk=pk).smiles)
+    out_data = get_my_graph(smiles)
+    return HttpResponse(json.dumps(out_data))
