f63df18e22db653c1a0d37d684739d38a8f070de
Anthony Bradley <anthony.richard.bradley@gmail.com>
2018-06-14 19:36:57 +0100

    Update views.py

--- scoring/views.py
+++ scoring/views.py
@@ -66,6 +66,6 @@ class MolGroupView(viewsets.ModelViewSet):
 
 def gen_conf_from_vect(request):
     input_vector = request.POST["INPUT_VECTOR"]
-    input_smiles = request.POST["INPUT_SMILES"]
+    input_smiles = request.POST.getlist("INPUT_SMILES")
     input_mol_block = request.POST["INPUT_MOL_BLOCK"]
     return HttpResponse(json.dumps(generate_confs_for_vector(input_vector, input_smiles, input_mol_block)))
