d3bb0cc250a5e071c9ffd077ccafeadbc7382825
abradle <you@example.com>
2018-06-15 13:53:32 +0100

    Add needed wildcard

--- viewer/urls.py
+++ viewer/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r"^react/$", views.react, name="react"),
+    url(r"^react/*", views.react, name="react"),
     url(r"^img_from_smiles/$", views.img_from_smiles, name="img_from_smiles"),
 ]
