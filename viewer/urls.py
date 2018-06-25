0d998e86902303acf89f6dc086a676e1288ad78c
abradle <you@example.com>
2018-06-25 14:21:11 +0100

    Added talking to the Enamine database

--- viewer/urls.py
+++ viewer/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r"^react/*", views.react, name="react"),
     url(r"^img_from_smiles/$", views.img_from_smiles, name="img_from_smiles"),
+    url(r"^sim_search/$", views.similarity_search, name="sim_search"),
 ]
