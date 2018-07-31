3c17504fc06958dc0f81d2d52c2d5259a83ac6b1
abradle <you@example.com>
2018-07-31 07:58:08 +0100

    Debug

--- media_serve/urls.py
+++ media_serve/urls.py
@@ -2,6 +2,4 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [
-    url(r"^pdbs/(?P<file_path>[\w\-]+)/$", views.prot_download, name="get_protein")
-]
+urlpatterns = [url(r"^pdbs/(?P<file_path>.+)", views.prot_download, name="get_protein")]
