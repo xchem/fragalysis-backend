d4cc23bad954b55d3cde8af263d66988a5e52c04
abradle <you@example.com>
2018-07-31 07:48:21 +0100

    Debug

--- media_serve/urls.py
+++ media_serve/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [url(r"^pdbs/$", views.prot_download, name="get_protein")]
+urlpatterns = [
+    url(r"^pdbs/(?P<file_path>[\w\-]+)/$", views.prot_download, name="get_protein")
+]
--- media_serve/views.py
+++ media_serve/views.py
@@ -2,12 +2,10 @@ from api.utils import get_response
 from viewer.models import Protein
 
 
-def prot_download(request, input_string):
+def prot_download(request, file_path):
     model = Protein
     permission = "target_id__project_id"
     field_name = "pdb_info"
     content_type = "application/x-pilot"
     prefix = "/pdbs/"
-    return get_response(
-        model, permission, field_name, content_type, prefix, input_string
-    )
+    return get_response(model, permission, field_name, content_type, prefix, file_path)
