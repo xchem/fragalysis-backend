25c6af6dbd9676d82c83cc834e651cf156791127
abradle <you@example.com>
2018-08-20 14:14:08 +0100

    Bugfix

--- tests/test_api.py
+++ tests/test_api.py
@@ -346,10 +346,10 @@ class APIUrlsTestCase(TestCase):
         # Test the login can access
         response = self.client.get(url_base + "/targets/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, not_secret_target_data)
+        self.assertEqual(response.json(), not_secret_target_data)
         self.client.login(
             username=self.user_two.username, password=self.user_two.password
         )
         response = self.client.get(url_base + "/targets/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, secret_target_data)
+        self.assertEqual(response.json(), secret_target_data)
