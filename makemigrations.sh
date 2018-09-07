affaf29beace57abd78d673e43185d5415f41b98
abradle <you@example.com>
2018-09-07 08:00:21 +0100

    Remove loader - refactored out to https://github.com/xchem/fragalysis-loader

--- makemigrations.sh
+++ makemigrations.sh
@@ -9,8 +9,6 @@ python /code/manage.py makemigrations scoring hypothesis hotspots xchem_db
 python /code/manage.py migrate
 python /code/manage.py makemigrations
 python /code/manage.py migrate  # Apply database migrations - weird order is due to https://stackoverflow.com/questions/31417470/django-db-utils-programmingerror-relation-app-user-does-not-exist-during-ma
-echo "Running loader..."
-python loader.py
 echo "Running collectstatic..."
 python manage.py collectstatic --clear --noinput -v 0 # clearstatic files
 python manage.py collectstatic --noinput -v 0 # collect static files
