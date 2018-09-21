8344058df9e104ba201a5aa454262715f13959f7
abradle <you@example.com>
2018-09-21 15:02:40 +0100

    Don't clear static content

--- makemigrations.sh
+++ makemigrations.sh
@@ -10,5 +10,4 @@ python /code/manage.py migrate
 python /code/manage.py makemigrations
 python /code/manage.py migrate  # Apply database migrations - weird order is due to https://stackoverflow.com/questions/31417470/django-db-utils-programmingerror-relation-app-user-does-not-exist-during-ma
 echo "Running collectstatic..."
-python manage.py collectstatic --clear --noinput -v 0 # clearstatic files
 python manage.py collectstatic --noinput -v 0 # collect static files
