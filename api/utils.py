70cc8605cba9caaedc211b96858bb8a2822a8194
Anthony Bradley <anthony.richard.bradley@gmail.com>
2018-09-22 18:57:39 +0100

    Update the drawing code to consider bond highlights
    * Update the drawing code to consider bond highlights

--- api/utils.py
+++ api/utils.py
@@ -6,7 +6,20 @@ from django.http import HttpResponse
 from rdkit import Chem
 from rdkit.Chem import AllChem, Draw
 from rdkit.Chem.Draw.MolDrawing import DrawingOptions
+from rdkit.Chem.Draw import rdMolDraw2D
 from rest_framework.authtoken.models import Token
+from frag.utils.network_utils import get_fragments, canon_input
+
+ISO_COLOUR_MAP = {
+    100: (1, 0, 0),
+    101: (0, 1, 0),
+    102: (0, 0, 1),
+    103: (1, 0, 1),
+    104: (1, 1, 0),
+    105: (0, 1, 1),
+    106: (0.5, 0.5, 0.5),
+    107: (1, 0.5, 1),
+}
 
 
 def get_token(request):
@@ -42,7 +55,16 @@ def _transparentsvg(svg):
     return '<?xml version="1.0" encoding="UTF-8"?>' + ET.tostring(tree).strip()
 
 
-def draw_mol(smiles, height=200, width=200, img_type=None):
+def draw_mol(
+    smiles,
+    height=200,
+    width=200,
+    img_type=None,
+    highlightAtoms=[],
+    atomcolors=[],
+    highlightBonds=[],
+    bondcolors={},
+):
     """
     Draw a molecule from a smiles
     :param smiles: the SMILES to render
@@ -60,10 +82,12 @@ def draw_mol(smiles, height=200, width=200, img_type=None):
     if not width:
         width = 200
     if img_type == "png":
-        options = DrawingOptions()
-        # options.defaultColor = (1, 1, 1)
-        options.bondLineWidth = 6
-        img = Draw.MolToImage(mol, options=options)
+        img = Draw.MolToImage(
+            mol,
+            options=options,
+            highlightBonds=highlightBonds,
+            highlightBondColors=bondcolors,
+        )
         img = img.convert("RGBA")
         datas = img.getdata()
         newData = []
@@ -77,21 +101,58 @@ def draw_mol(smiles, height=200, width=200, img_type=None):
         img.save(response, "PNG")
         return response
     else:
-        drawer = Draw.MolDraw2DSVG(height, width)
+        drawer = rdMolDraw2D.MolDraw2DSVG(height, width)
+        drawopt = drawer.drawOptions()
+        drawopt.clearBackground = False
+        drawer.DrawMolecule(
+            mol,
+            highlightAtoms=highlightAtoms,
+            highlightAtomColors=atomcolors,
+            highlightBonds=highlightBonds,
+            highlightBondColors=bondcolors,
+        )
         drawer.DrawMolecule(mol)
         drawer.FinishDrawing()
-        return _transparentsvg(drawer.GetDrawingText().replace("svg:", ""))
+        return drawer.GetDrawingText().replace("svg:", "")
+
+
+def parse_vectors(vector_list):
+    return [int(x) for x in vector_list.split(",")]
 
 
 def get_params(smiles, request):
+    try:
+        smiles = canon_input(smiles)
+    except:
+        smiles = ""
     height = None
+    bond_id_list = []
+    highlightBondColors = {}
     if "height" in request.GET:
         height = int(request.GET["height"])
     width = None
     if "width" in request.GET:
         width = int(request.GET["width"])
+    if "isotopes" in request.GET:
+        # Get the vector
+        isotopes = parse_vectors(request.GET["isotopes"])
+        fragments, frag_map = get_fragments(
+            Chem.MolFromSmiles(smiles), get_index_iso_map=True
+        )
+        for iso in isotopes:
+            bond_ids = frag_map[iso]
+            bond_id_list.extend(bond_ids)
+            for bond_id in bond_ids:
+                highlightBondColors[bond_id] = ISO_COLOUR_MAP[iso]
     img_type = request.GET.get("img_type", None)
-    get_mol = draw_mol(smiles, width=width, height=height, img_type=img_type)
+    get_mol = draw_mol(
+        smiles,
+        width=width,
+        height=height,
+        img_type=img_type,
+        highlightBonds=bond_id_list,
+        bondcolors=highlightBondColors,
+    )
     if type(get_mol) == HttpResponse:
         return get_mol
     return HttpResponse(get_mol)
