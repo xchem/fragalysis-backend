3eda5936a1b3a06cb727c0d649007c4632d103d8
abradle <you@example.com>
2018-09-22 12:44:48 +0100

    Update the drawing code to consider bond highlights

--- api/utils.py
+++ api/utils.py
@@ -7,6 +7,18 @@ from rdkit import Chem
 from rdkit.Chem import AllChem, Draw
 from rdkit.Chem.Draw.MolDrawing import DrawingOptions
 from rest_framework.authtoken.models import Token
+from frag.utils.network_utils import get_fragments, canon_input
+
+ISO_COLOUR_MAP = {
+    100: (1, 0, 0),
+    101: (0, 1, 0),
+    102: (0, 0, 1),
+    103: (1, 0, 1),
+    104: (1, 1, 0),
+    105: (0, 1, 1),
+    106: (0.5, 0.5, 0.5),
+    107: (1, 0.5, 1),
+}
 
 
 def get_token(request):
@@ -42,7 +54,7 @@ def _transparentsvg(svg):
     return '<?xml version="1.0" encoding="UTF-8"?>' + ET.tostring(tree).strip()
 
 
-def draw_mol(smiles, height=200, width=200, img_type=None):
+def draw_mol(smiles, height=200, width=200, img_type=None, **kwargs):
     """
     Draw a molecule from a smiles
     :param smiles: the SMILES to render
@@ -50,6 +62,12 @@ def draw_mol(smiles, height=200, width=200, img_type=None):
     :param width: the width in px
     :return: an SVG as a string of the inage
     """
+    # Set the options here
+    options = DrawingOptions()
+    options.atomLabelFontSize = 100
+    options.dotsPerAngstrom = 200
+    options.bondLineWidth = 6.0
+    #
     mol = Chem.MolFromSmiles(smiles)
     if mol is None:
         return "None Mol"
@@ -60,10 +78,7 @@ def draw_mol(smiles, height=200, width=200, img_type=None):
     if not width:
         width = 200
     if img_type == "png":
-        options = DrawingOptions()
-        # options.defaultColor = (1, 1, 1)
-        options.bondLineWidth = 6
-        img = Draw.MolToImage(mol, options=options)
+        img = Draw.MolToImage(mol, options=options, **kwargs)
         img = img.convert("RGBA")
         datas = img.getdata()
         newData = []
@@ -83,15 +98,39 @@ def draw_mol(smiles, height=200, width=200, img_type=None):
         return _transparentsvg(drawer.GetDrawingText().replace("svg:", ""))
 
 
+def parse_vectors(vector_list):
+    return [int(x) for x in vector_list.split(",")]
+
+
 def get_params(smiles, request):
     height = None
+    bond_id_list = []
+    highlightBonds = []
+    highlightBondColors = {}
     if "height" in request.GET:
         height = int(request.GET["height"])
     width = None
     if "width" in request.GET:
         width = int(request.GET["width"])
+    if "isotopes" in request.GET:
+        # Get the vector
+        isotopes = parse_vectors(request.GET["isotopes"])
+        fragments, frag_map = get_fragments(canon_input(smiles), get_index_iso_map=True)
+        for iso in isotopes:
+            bond_ids = frag_map[iso]
+            bond_id_list.extend(bond_ids)
+            for bond_id in bond_ids:
+                highlightBondColors[bond_id] = ISO_COLOUR_MAP[iso]
+        highlightBonds = bond_id_list
     img_type = request.GET.get("img_type", None)
-    get_mol = draw_mol(smiles, width=width, height=height, img_type=img_type)
+    get_mol = draw_mol(
+        smiles,
+        width=width,
+        height=height,
+        img_type=img_type,
+        highlightBonds=highlightBonds,
+        highlightBondColors=highlightBondColors,
+    )
     if type(get_mol) == HttpResponse:
         return get_mol
     return HttpResponse(get_mol)
