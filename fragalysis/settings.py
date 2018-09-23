b5a9970bf8cc982d0e843bbaf527e7d342092569
abradle <you@example.com>
2018-09-23 20:55:32 +0100

    Add sentry support

--- fragalysis/settings.py
+++ fragalysis/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://8a0575728eec4bfa887f55f31b9a01f5@sentry.io/1286652",
+    integrations=[DjangoIntegration()],
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
