67f8122d9429f6bb193a63fbcaaaa3123fad0578
reskyner <reskyner@googlemail.com>
2018-10-17 15:47:33 +0100

    temporary patch to fix index error on some data - changing to always true messes up vector display

--- viewer/serializers.py
+++ viewer/serializers.py
@@ -170,7 +170,11 @@ class VectorsSerializer(serializers.ModelSerializer):
 
     def get_vectors(self, obj):
         out_data = {}
-        out_data["3d"] = get_3d_vects_for_mol(obj.sdf_info)
+        try:
+            out_data["3d"] = get_3d_vects_for_mol(obj.sdf_info, iso_labels=False)
+        # temporary patch
+        except IndexError:
+            out_data["3d"] = get_3d_vects_for_mol(obj.sdf_info, iso_labels=True)
         out_data["indices"] = get_vect_indices_for_mol(obj.sdf_info)
         return out_data
 
