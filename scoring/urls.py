81422e9dbfcc0c31213e27a9f9e4b730437f83b5
reskyner <reskyner@googlemail.com>
2018-10-19 15:04:16 +0100

    typo

--- scoring/urls.py
+++ scoring/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r"^gen_conf_from_vect/$", views.gen_conf_from_vect, name="gen_conf_from_vect"),
-    url(r"get_current_user_id/$", views.get_current_user_id, name="get_current_user_id")
+    url(r"^get_current_user_id/$", views.get_current_user_id, name="get_current_user_id")
 ]
