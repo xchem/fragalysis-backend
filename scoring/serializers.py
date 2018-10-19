2fdc9372af23daa18da822a6fde3b3c0e20229e6
reskyner <reskyner@googlemail.com>
2018-10-19 13:34:07 +0100

    back to just user_id

--- scoring/serializers.py
+++ scoring/serializers.py
@@ -15,7 +15,7 @@ class ViewSceneSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ViewScene
-        fields = ("id", "uuid", "title", "scene", "user_id", "user_id__username", "created", "modified")
+        fields = ("id", "uuid", "title", "scene", "user_id", "created", "modified")
 
 
 class ProtChoiceSerializer(serializers.ModelSerializer):
