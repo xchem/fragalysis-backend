43e6b5648f50cfe3212909604e5f747a0b6bea70
reskyner <reskyner@googlemail.com>
2018-12-17 15:05:11 +0000

    make bound pdbs downloadable

--- media_serve/urls.py
+++ media_serve/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [url(r"^pdbs/(?P<file_path>.+)", views.prot_download, name="get_protein"),
-               url(r"maps/(?P<file_path>.+)", views.map_download, name='get_map')]
+               url(r"^pdbs/(?P<file_path>.+)", views.bound_prot_download, name="get_bound_protein"),
+               url(r"^maps/(?P<file_path>.+)", views.map_download, name='get_map')]
