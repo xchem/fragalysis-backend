b38e1e612408564062daaacc4957eec5e31d0ef4
reskyner <reskyner@googlemail.com>
2019-02-14 09:34:03 +0000

    test __in

--- fragspect/serializers.py
+++ fragspect/serializers.py
@@ -29,61 +29,61 @@ from rest_framework import serializers
 class FragspectSerializer(serializers.ModelSerializer):
     site_number = serializers.SerializerMethodField()
     event_number = serializers.SerializerMethodField()
-    # codes = serializers.SerializerMethodField()
+    codes = serializers.SerializerMethodField()
     lig_id = serializers.SerializerMethodField()
     target_name = serializers.SerializerMethodField()
     smiles = serializers.SerializerMethodField()
-    # pandda_model_found = serializers.SerializerMethodField()
-    # resolution = serializers.SerializerMethodField()
+    resolution = serializers.SerializerMethodField()
     space_group = serializers.SerializerMethodField()
     unit_cell = serializers.SerializerMethodField()
 
-    def get_site_number(self, obj):
-        return [e.site.site for e in PanddaEvent.objects.filter(crystal__in=obj)]
-
-    def get_event_number(self, obj):
-        return [e.event for e in PanddaEvent.objects.filter(crystal__in=obj)]
-
-    # def get_code(self, obj):
-    #     pass
-
-    def get_lig_id(self, obj):
-        return [e.lig_id for e in PanddaEvent.objects.filter(crystal__in=obj)]
-
-    def get_target_name(self, obj):
-        return [e.crystal.target.target_name for e in PanddaEvent.objects.filter(crystal__in=obj)]
-
-    def get_smiles(self, obj):
-        return [e.crystal.compound.smiles for e in PanddaEvent.objects.filter(crystal__in=obj)]
-
-    # def get_pandda_model_found(self, obj):
-    #     pass
-
-    # def get_resolution(self, obj):
-    #     try:
-    #         return [PanddaEventStats.objects.get(event=e).resolution for e in PanddaEvent.objects.filter(crystal=obj)]
-    #     except:
-
-
-    # def get_space_group(self, obj):
-    #     return [DataProcessing.objects.get(crystal_name=e.crystal).spacegroup
-    #             for e in PanddaEvent.objects.filter(crystal=obj)]
+    # def get_site_number(self, obj):
+    #     return [e.site.site for e in PanddaEvent.objects.filter(crystal__in=obj)]
     #
-    # def get_unit_cell(self, obj):
-    #     return [DataProcessing.objects.get(crystal_name=e.crystal).unit_cell
-    #             for e in PanddaEvent.objects.filter(crystal=obj)]
+    # def get_event_number(self, obj):
+    #     return [e.event for e in PanddaEvent.objects.filter(crystal__in=obj)]
+    #
+    # # def get_code(self, obj):
+    # #     pass
+    #
+    # def get_lig_id(self, obj):
+    #     return [e.lig_id for e in PanddaEvent.objects.filter(crystal__in=obj)]
+    #
+    # def get_target_name(self, obj):
+    #     return [e.crystal.target.target_name for e in PanddaEvent.objects.filter(crystal__in=obj)]
+    #
+    # def get_smiles(self, obj):
+    #     return [e.crystal.compound.smiles for e in PanddaEvent.objects.filter(crystal__in=obj)]
+    #
+    # # def get_pandda_model_found(self, obj):
+    # #     pass
+    #
+    # # def get_resolution(self, obj):
+    # #     try:
+    # #         return [PanddaEventStats.objects.get(event=e).resolution for e in PanddaEvent.objects.filter(crystal=obj)]
+    # #     except:
+    #
+    #
+    # # def get_space_group(self, obj):
+    # #     return [DataProcessing.objects.get(crystal_name=e.crystal).spacegroup
+    # #             for e in PanddaEvent.objects.filter(crystal=obj)]
+    # #
+    # # def get_unit_cell(self, obj):
+    # #     return [DataProcessing.objects.get(crystal_name=e.crystal).unit_cell
+    # #             for e in PanddaEvent.objects.filter(crystal=obj)]
 
     class Meta:
         Model = Crystal
         fields = (
             'site_number',
             'event_number',
+            'codes',
             'lig_id',
             'target_name',
             'smiles',
-            # 'resolution',
-            # 'spacegroup',
-            # 'unit_cell',
+            'resolution',
+            'spacegroup',
+            'unit_cell',
         )
 
 
