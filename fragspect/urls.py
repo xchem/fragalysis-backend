508148ab2e13b18b9ad6074dfcf3346e0b4c9622
reskyner <reskyner@googlemail.com>
2019-02-14 09:46:29 +0000

    smiles example

--- fragspect/urls.py
+++ fragspect/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import include, url
 from rest_framework.authtoken import views as drf_views
 from rest_framework.routers import DefaultRouter
 
-from fragspect.views import FragspectView
+from fragspect.views import CrystalView
 
 router = DefaultRouter()
 
-router.register(r'list', FragspectView)
+router.register(r'list', CrystalView)
 
 urlpatterns = [
     url(r"^", include(router.urls)),
