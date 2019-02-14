1579a598d5bcae111027afa4c7a4cd33135aaf44
reskyner <reskyner@googlemail.com>
2019-02-14 13:03:12 +0000

    test with xchem_db serializer

--- fragspect/urls.py
+++ fragspect/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import include, url
 from rest_framework.authtoken import views as drf_views
 from rest_framework.routers import DefaultRouter
 
-from fragspect.views import CrystalView
+from fragspect.views import FragspectView
 
 router = DefaultRouter()
 
-router.register(r'list', CrystalView)
+router.register(r'frag', FragspectView)
 
 urlpatterns = [
     url(r"^", include(router.urls)),
--- fragspect/views.py
+++ fragspect/views.py
@@ -1,10 +1,10 @@
 from xchem_db.models import Crystal
-from xchem_db.serializers import FragspectSerializer
+from fragspect.serializers import FragspectSerializer
 
 from api.security import ISpyBSafeQuerySet
 
 
-class FragalysisView(ISpyBSafeQuerySet):
+class FragspectView(ISpyBSafeQuerySet):
     queryset = Crystal.objects.filter()
     filter_fields = ("target__target_name",)
     filter_permissions = "visit__proposal"
