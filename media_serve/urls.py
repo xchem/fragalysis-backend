bf73657f1a4fee53601db02df638231cf66e429c
reskyner <reskyner@googlemail.com>
2019-03-07 05:05:08 +0000

    bound download - change where served

--- media_serve/urls.py
+++ media_serve/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [url(r"^pdbs/(?P<file_path>.+)", views.prot_download, name="get_protein"),
-               url(r"^pdbs/(?P<file_path>.+)", views.bound_prot_download, name="get_bound_protein"),
+               url(r"^bound/(?P<file_path>.+)", views.bound_download, name="get_bound"),
                url(r"^maps/(?P<file_path>.+)", views.map_download, name='get_map')]
--- media_serve/views.py
+++ media_serve/views.py
@@ -20,7 +20,7 @@ def prot_download(request, file_path):
     return ispy_b_static.get_response()
 
 
-def bound_prot_download(request, file_path):
+def bound_download(request, file_path):
     """
     Download a protein by nginx redirect
     :param request: the initial request
@@ -33,7 +33,7 @@ def bound_prot_download(request, file_path):
     ispy_b_static.permission_string = "target_id__project_id"
     ispy_b_static.field_name = "bound_info"
     ispy_b_static.content_type = "application/x-pilot"
-    ispy_b_static.prefix = "/pdbs/"
+    ispy_b_static.prefix = "/bound/"
     ispy_b_static.input_string = file_path
     return ispy_b_static.get_response()
 
