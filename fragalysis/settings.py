a8dc32ee6140b5b6e55bd13ca93206270d75e5eb
Tibor Postek <roman.pis@compote.eu>
2020-03-25 06:46:58 +0100

    #1 enable to create project and snapshot for not logged in users

--- fragalysis/settings.py
+++ fragalysis/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
