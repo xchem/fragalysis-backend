0e2f2a5835bf532ac898c07130053c38e16e4c47
uzw12877 <uzw12877@diamrm5062.local>
2020-04-28 11:14:18 +0100

    try to fix the ref_pdb checker

--- viewer/sdf_check.py
+++ viewer/sdf_check.py
@@ -53,7 +53,7 @@ def check_pdb(mol, validate_dict):
     # Check if pdb filepath given and exists
     test_fp = mol.GetProp('ref_pdb')
 
-    if not test_fp.endswith('_0') or not test_fp.endswith('.pdb'):
+    if not '_0' in test_fp or not '.pdb' in test_fp:
         validate_dict = add_warning(molecule_name=mol.GetProp('_Name'),
                                     field='ref_pdb',
                                     warning_string="illegal pdb assingment for " + str(test_fp),
