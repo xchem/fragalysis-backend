ee2b0881ab968e999b045f1688fa2a9b5c3673ce
uzw12877 <uzw12877@diamrm5062.local>
2020-04-28 12:38:28 +0100

    upload or validate

--- viewer/forms.py
+++ viewer/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class CSetForm(forms.Form):
     target_name = forms.CharField(label='Target', max_length=100)
     sdf_file = forms.FileField()
-    submit_choice = forms.ChoiceField(widget=forms.RadioSelect(), choices=[(0, 'validate'), (1, 'upload')])
+    submit_choice = forms.ChoiceField(widget=forms.RadioSelect, choices=[(0, 'validate'), (1, 'upload')])
