cc62f3a9eab8fc2c7275182b6844e02510e09832
uzw12877 <uzw12877@diamrm5062.local>
2020-04-29 11:10:53 +0100

    add zip file logic

--- viewer/forms.py
+++ viewer/forms.py
@@ -10,7 +10,7 @@ CHOICES = [
 class CSetForm(forms.Form):
     target_name = forms.CharField(label='Target', max_length=100)
     sdf_file = forms.FileField(label='All compounds sdf (.sdf)')
-    pdb_zip = forms.FileField(blank=True, label='PDB files (.zip)')
+    pdb_zip = forms.FileField(required=False, label='PDB files (.zip)')
     submit_choice = forms.CharField(widget=forms.RadioSelect(choices=CHOICES))
 
     # def clean_zipfile(self):
