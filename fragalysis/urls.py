7b83bff5e2623d32fc367e177c6a16c37ca2b999
uzw12877 <uzw12877@diamrm5062.local>
2020-05-06 21:32:22 +0100

    try adding namespace

--- fragalysis/urls.py
+++ fragalysis/urls.py
@@ -20,7 +20,7 @@ from django.views.generic.base import RedirectView
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    url(r"^celery-progress/", include('celery_progress.urls')),
+    url(r"^celery-progress/", include('celery_progress.urls'), namespace="celery_progress"),
     url(r"^admin/", admin.site.urls),
     url(r"^viewer/", include("viewer.urls")),
     url(r"^network/", include("network.urls")),
