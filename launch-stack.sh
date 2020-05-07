c700a111b8e751a176360d8f9d890b4549137377
uzw12877 <uzw12877@diamrm5062.local>
2020-05-07 01:42:35 +0100

    package changes

--- launch-stack.sh
+++ launch-stack.sh
@@ -42,3 +42,7 @@ echo "Testing nginx config..."
 nginx -t
 echo "Running nginx..."
 nginx
+echo "starting redis server..."
+redis-server &
+echo "starting celery daemon..."
+celery -A fragalysis worker -l info &
