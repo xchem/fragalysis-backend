1ce573f2ab179694ba8b0aa44942a4ff16bddda5
root <root@stack-1.stack.staging-stack.svc.cluster.local>
2020-06-15 14:08:51 +0000

    edit makemigrations.sh to include fix for migration fail

--- makemigrations.sh
+++ makemigrations.sh
@@ -2,7 +2,11 @@
 echo "Running migrations..."
 cd /code
 # initial migrations for existing stack
-python manage.py migrate
+python manage.py migrate viewer 0001_initial
+python manage.py migrate viewer 0002_auto_20200615_1322
+python manage.py migrate auth
+python manage.py migrate xchem_db
+python manage.py migrate scoring hypothesis hotspots
 # make and apply new migrations not already in stack
 python /code/manage.py makemigrations auth
 python /code/manage.py migrate
