aad017ea8cbeaf41c462b1aeab8523d6f91802e9
Rachael Skyner <reskyner@googlemail.com>
2020-09-22 16:27:21 +0200

    clean up makemigrations.sh

--- makemigrations.sh
+++ makemigrations.sh
@@ -2,20 +2,16 @@
 echo "Running migrations..."
 cd /code
 # initial migrations for existing stack
-python manage.py migrate viewer 
 python manage.py migrate auth
-python manage.py migrate xchem_db
-python manage.py migrate scoring hypothesis hotspots
+python manage.py migrate viewer
+python manage.py migrate scoring hypothesis
+
 # make and apply new migrations not already in stack
 python /code/manage.py makemigrations auth
 python /code/manage.py migrate
-python /code/manage.py makemigrations xchem_db
-python /code/manage.py migrate --fake-initial
-python /code/manage.py makemigrations viewer
-python /code/manage.py migrate  --fake-initial
-python /code/manage.py makemigrations scoring hypothesis hotspots
-python /code/manage.py migrate  --fake-initial
+
 python /code/manage.py makemigrations
 python /code/manage.py migrate  # Apply database migrations - weird order is due to https://stackoverflow.com/questions/31417470/django-db-utils-programmingerror-relation-app-user-does-not-exist-during-ma
+
 echo "Running collectstatic..."
 python manage.py collectstatic --noinput -v 0 # collect static files
