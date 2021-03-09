5d43da24e72c558cec924240ae612d72a0d7362d
Duncan Peacock <duncan_peacock@yahoo.co.uk>
2021-03-09 14:48:49 +0100

    On logon, search for username if no users found for email

--- fragalysis/auth.py
+++ fragalysis/auth.py
@@ -14,6 +14,23 @@ class KeycloakOIDCAuthenticationBackend(OIDCAuthenticationBackend):
         user.save()
         return user
 
+    def filter_users_by_claims(self, claims):
+        """ Return all users matching the specified email.
+            If nothing found matching the email, then try the username
+        """
+        email = claims.get('email')
+        preferred_username = claims.get('preferred_username')
+
+        if not email:
+            return self.UserModel.objects.none()
+        users = self.UserModel.objects.filter(email__iexact=email)
+
+        if len(users) < 1:
+            if not preferred_username:
+                return self.UserModel.objects.none()
+            users = self.UserModel.objects.filter(username__iexact=preferred_username)
+        return users
+
     def update_user(self, user, claims):
         user.first_name = claims.get('given_name', '')
         user.last_name = claims.get('family_name', '')
