9f76a9d1ce66fdbfb2781bd96cd2f47a64f7b222
Waztom <waztom@gmail.com>
2022-02-08 17:10:38 +0000

    Removed dummy vars from test entry

--- test_entry.sh
+++ test_entry.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 /bin/bash /code/makemigrations.sh
 export ISPYB_FLAG=""
-# Dummy env variables for test
-export IBM_API_KEY=""
-export MANIFOLD_API_KEY=""
-export MCULE_API_KEY=""
-export SENDGRID_API_KEY=""
 
 python /code/manage.py test --no-input
