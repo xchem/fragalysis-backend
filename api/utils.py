5c961de9579a290f764a1d2bf494d2bc1b505e2e
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2022-02-15 14:32:42 +0000

    #794 font calculated based on image scaling

--- api/utils.py
+++ api/utils.py
@@ -86,7 +86,107 @@ def highlight_diff(prb_mol, ref_mol, width, height):
     return svg
 
 
-def draw_mol(
+def calc_bounds(conformer):
+    """
+    Calculate the X,Y bounds of a molecule, the minimum and maximum X and Y coordinates of any atom in the molecule.
+    It is assumed that the molecule already has 2D coordinates.
+
+    :param mol:
+    :return: X and Y bounds as lists of length 2
+    """
+    x = [0, 0]
+    y = [0, 0]
+    num = conformer.GetOwningMol().GetNumAtoms()
+    for i in range(num):
+        pos = conformer.GetAtomPosition(i)
+        if pos.x < x[0]:
+            x[0] = pos.x
+        if pos.x > x[1]:
+            x[1] = pos.x
+        if pos.y < y[0]:
+            y[0] = pos.y
+        if pos.y > y[1]:
+            y[1] = pos.y
+    return x, y
+
+
+def calc_dims(x, y):
+    """
+    Given the X and Y bounds generated with calc_bounds() calculate the X and Y dimensions
+    :param x:
+    :param y:
+    :return: The X and Y dimensions
+    """
+    xd = x[1] - x[0]
+    yd = y[1] - y[0]
+    return xd, yd
+
+
+def draw_mol(smiles, height=49, width=150, bondWidth=1, scaling=1.0, img_type=None,
+             highlightAtoms=[], atomcolors=[], highlightBonds=[], bondcolors={}, mol=None):
+    """
+    Generate a SVG image of a molecule specified as SMILES
+    :param smiles: The molecuels as SMILES
+    :param the height in px
+    :param width: the width in px
+    :param bondWidth:
+    :param scaling:
+    :param img_type: Ignored. Kept for now for backwards compatibility
+    :param highlightAtoms:
+    :param atomcolors:
+    :param highlightBonds:
+    :param bondcolors:
+    :param mol: an optional mol to use instead of the smiles. Kept for now for backwards compatibility
+    :return:
+    """
+
+    if not mol:
+        mol = Chem.MolFromSmiles(smiles)
+    if mol is None:
+        return "None Mol"
+
+    Chem.Kekulize(mol)
+    if mol.GetNumConformers() == 0:
+        idx = AllChem.Compute2DCoords(mol)
+        conformer = mol.GetConformer(idx)
+    else:
+        conformer = mol.GetConformer(-1) # the default conformer
+
+    # Do some maths to determine the optimal font.
+    # If you want to influence this use the scaling parameter.
+    x, y = calc_bounds(conformer)
+    dim_x, dim_y = calc_dims(x, y)
+    scale_x = width / dim_x
+    scale_y = height / dim_y
+    scale = min(scale_x, scale_y)
+    font = max(round(scale * scaling), 6)
+
+    # Now we can generate the drawing
+    rdMolDraw2D.PrepareMolForDrawing(mol, wedgeBonds=False)
+    drawer = rdMolDraw2D.MolDraw2DSVG(width, height)
+
+    drawopt = drawer.drawOptions()
+    drawopt.clearBackground = False
+    drawopt.padding = 0.05
+    drawopt.bondLineWidth = bondWidth
+    drawopt.minFontSize = font
+    drawopt.maxFontSize = font
+    drawopt.additionalAtomLabelPadding = 0.15
+
+    drawer.DrawMolecule(
+        mol,
+        highlightAtoms=highlightAtoms,
+        highlightAtomColors=atomcolors,
+        highlightBonds=highlightBonds,
+        highlightBondColors=bondcolors,
+    )
+
+    drawer.FinishDrawing()
+    return drawer.GetDrawingText()
+
+
+# This old method kept for reference. Remove it once we are happy with the new method
+def draw_mol_old(
         smiles,
         height=49,
         width=150,
