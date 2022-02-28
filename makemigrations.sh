5c2aaa67f63ebc0c93f89a82ef56fae0a8630482
Waztom <waztom@gmail.com>
2022-02-28 12:43:10 +0000

    remove black and update to use python3 cmd

--- makemigrations.sh
+++ makemigrations.sh
@@ -2,16 +2,16 @@
 echo "Running migrations..."
 cd /code
 # initial migrations for existing stack
-python manage.py migrate auth
-python manage.py migrate viewer
-python manage.py migrate scoring hypothesis car
+python3 manage.py migrate auth
+python3 manage.py migrate viewer
+python3 manage.py migrate scoring hypothesis car
 
 # make and apply new migrations not already in stack
-python /code/manage.py makemigrations auth
-python /code/manage.py migrate
+python3 /code/manage.py makemigrations auth
+python3 /code/manage.py migrate
 
-python /code/manage.py makemigrations
-python /code/manage.py migrate  # Apply database migrations - weird order is due to https://stackoverflow.com/questions/31417470/django-db-utils-programmingerror-relation-app-user-does-not-exist-during-ma
+python3 /code/manage.py makemigrations
+python3 /code/manage.py migrate  # Apply database migrations - weird order is due to https://stackoverflow.com/questions/31417470/django-db-utils-programmingerror-relation-app-user-does-not-exist-during-ma
 
 echo "Running collectstatic..."
-python manage.py collectstatic --noinput -v 0 # collect static files
+python3 manage.py collectstatic --noinput -v 0 # collect static files
