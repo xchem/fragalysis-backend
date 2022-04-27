c3a8580e60da34d07a8394655f410ba3e692f111
Waztom <waztom@gmail.com>
2022-04-27 13:06:24 +0000

    Revert back to form type for POST

--- car/api.py
+++ car/api.py
@@ -302,7 +302,7 @@ class BatchViewSet(viewsets.ModelViewSet):
     def canonicalizesmiles(self, request, pk=None):
         check_services()
         if request.POST.get("smiles"):
-            smiles = request.data["smiles"]
+            smiles = request.POST.getlist("smiles")
             task = canonicalizeSmiles.delay(smiles=smiles)
             data = {"task_id": task.id}
             return JsonResponse(data=data)
@@ -343,7 +343,7 @@ class BatchViewSet(viewsets.ModelViewSet):
     @action(methods=['post'], detail=False)
     def updatereactionsuccess(self, request, pk=None):
         if request.POST.get("reaction_ids"):
-            reaction_ids = request.data["reaction_ids"]
+            reaction_ids = request.POST.getlist("reaction_ids")
         if len(request.FILES) != 0:
             csvfile = request.FILES["csv_file"]
             reaction_ids = pd.read_csv(csvfile)["reaction_id"]
