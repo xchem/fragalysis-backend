9156e49a6d31572accf1d94480814a125eebe75c
Alan Christie <alan.christie@matildapeak.com>
2022-06-14 10:14:11 +0100

    Adds 'touch /code/logs/logfile.log'

--- launch-stack.sh
+++ launch-stack.sh
@@ -21,6 +21,7 @@ printf "$script" | python manage.py shell
 # Prepare log files and start outputting logs to stdout
 touch /srv/logs/gunicorn.log
 touch /srv/logs/access.log
+touch /code/logs/logfile.log
 tail -n 0 -f /srv/logs/*.log &
 echo "Starting Gunicorn...."
 cd /code
