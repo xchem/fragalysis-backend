24c858f444bbce71e7910f51ef00ef7e026f06cb
Alan Christie <alan.christie@matildapeak.com>
2023-07-25 17:23:58 +0100

    feat: Can now set celery broker and result backedn via ENV

--- fragalysis/settings.py
+++ fragalysis/settings.py
@@ -86,8 +86,8 @@ REST_FRAMEWORK = {
 }
 
 # CELERY STUFF
-CELERY_BROKER_URL = 'redis://redis:6379'
-CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379')
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_ALWAYS_EAGER = os.environ.get('CELERY_TASK_ALWAYS_EAGER', 'False').lower() in ['true', 'yes']
 CELERY_TASK_SERIALIZER = 'json'
