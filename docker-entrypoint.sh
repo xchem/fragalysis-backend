2a08a54bcc75d83c8ab7081a273afe64f1e5ad91
Alan Christie <alan.christie@matildapeak.com>
2023-07-27 15:26:12 +0100

    fix: Handles unexpected container roles

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,28 +6,26 @@ echo "+> CONTAINER_ROLE is ${CONTAINER_ROLE}"
 if [ "${CONTAINER_ROLE,,}" = "worker" ]; then
 
   # WORKER (at least one Pod will be a worker)
-  #
-  # This container is expected to act a
-  # Fragalysis Stack Celery Task Worker
   echo "+> Running as WORKER"
   /code/launch-worker.sh
 
 elif [ "${CONTAINER_ROLE,,}" = "beat" ]; then
 
   # BEAT (only one Pod will be a beat Pod)
-  #
-  # This container is expected to act a
-  # Fragalysis Stack Celery Beat Scheduler
   echo "+> Running as BEAT"
   /code/launch-beat.sh
 
-else
+elif [ "${CONTAINER_ROLE,,}" = "stack" ]; then
 
   # STACK (at least one Pod will be a stack)
-  #
-  # This image is expected to act as the
-  # Fragalysis Stack _first responder_ - i.e. the REST endpoint.
   echo "+> Running as STACK"
   /code/launch-stack.sh
 
+else
+  
+  # Unsupported Role! (this should never happen)
+  echo "+> CONTAINER_ROLE is not supported!"
+
 fi
+
+echo "-> Exiting..."
