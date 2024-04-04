bbb2be36151dbce7fe4f0437ab624771d4205295
Alan Christie <alan.christie@matildapeak.com>
2024-04-04 11:44:38 +0100

    fix: Fix while loop exit conditions

--- api/remote_ispyb_connector.py
+++ api/remote_ispyb_connector.py
@@ -142,7 +142,7 @@ class SSHConnector(Connector):
         # before giving up...
         connect_attempts = 0
         self.conn = None
-        while connect_attempts < PYMYSQL_OE_RECONNECT_ATTEMPTS:
+        while not self.conn and connect_attempts < PYMYSQL_OE_RECONNECT_ATTEMPTS:
             try:
                 self.conn = pymysql.connect(
                     user=db_user,
