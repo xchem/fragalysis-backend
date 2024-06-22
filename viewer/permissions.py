effeb12f9c85446ae89fa7da274f8f78934b503a
Alan Christie <alan.christie@matildapeak.com>
2024-06-22 07:36:55 +0100

    fix: Better has_object_permission

--- viewer/permissions.py
+++ viewer/permissions.py
@@ -2,6 +2,7 @@ import logging
 from typing import List
 
 from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 
 from api.security import ISpyBSafeQuerySet
 
@@ -17,17 +18,17 @@ class IsManyProposalMember(permissions.BasePermission):
     message: str = "Your authority to access this object has not been given"
 
     def has_object_permission(self, request, view, obj):
-        # Here we check that the user has access to the proposal(s)
-        # the object belongs to.
-        # What proposals does the Object belong to?
-        # The object's proposal record ID is stored in the filter_permissions field.
+        # Here we check that the user has access to one of the proposal(s)
+        # the object belongs to. The object's proposal record IDs (Many)
+        # is stored in the reference defined in its 'filter_permissions' field.
         object_proposals: List[str] = [
             p.title for p in getattr(obj, view.filter_permissions).all()
         ]
-        _LOGGER.info("obj.proposals=%s", object_proposals)
         # So - is any of the object's proposals in the user's list of proposals?
-        permission = _ISPYB_SAFE_QUERY_SET.user_is_member_of_any_given_proposals(
+        if not _ISPYB_SAFE_QUERY_SET.user_is_member_of_any_given_proposals(
             user=request.user, proposals=object_proposals
-        )
-        _LOGGER.info("permission=%s", permission)
-        return permission
+        ):
+            raise PermissionDenied(
+                detail="Your authority to access this object has not been given"
+            )
+        return True
