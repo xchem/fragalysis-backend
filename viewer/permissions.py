7cf186960f7f9c68a3992dff75a4109d18743d7d
Alan Christie <alan.christie@matildapeak.com>
2024-06-23 19:39:21 +0100

    docs: Doc tweak

--- viewer/permissions.py
+++ viewer/permissions.py
@@ -14,17 +14,18 @@ class IsManyProposalMember(permissions.BasePermission):
         # Users must be authenticated
         if not request.user.is_authenticated:
             return False
-        # Here we check that the user has access to one of the proposal(s)
-        # the object belongs to. The object's proposal record IDs (Many)
-        # is stored in the reference defined in its 'filter_permissions' field.
+        # Here we check that the user has access to any proposal the object belongs to.
+        # The object's proposal records (many in this case) are stored in the reference
+        # defined in the view's 'filter_permissions' property, e.g. 'project_id'.
         object_proposals = [
             p.title for p in getattr(obj, view.filter_permissions).all()
         ]
-        # So - is any of the object's proposals in the user's list of proposals?
+        # So - has the user been associated with any of the proposals for this object?
         if not _ISPYB_SAFE_QUERY_SET.user_is_member_of_any_given_proposals(
             user=request.user, proposals=object_proposals
         ):
             raise PermissionDenied(
                 detail="Your authority to access this object has not been given"
             )
+        # User is a member of at least one of the object's proposals...
         return True
