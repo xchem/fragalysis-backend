e841dd29207da946b3d5e14028807cf730beeb61
Kalev Takkis <ktakkis@informaticsmatters.com>
2025-10-15 08:58:35 +0100

    feat: endpoint for checking proposal permissions

--- api/urls.py
+++ api/urls.py
@@ -179,6 +179,8 @@ router.register(
 
 router.register("plot_data", viewer_views.PlotDataView, basename='plot_data')
 
+router.register("tas_stats", viewer_views.TASStatsView, basename='tas_stats')
+
 # Squonk Jobs
 router.register(
     "job_file_transfer", viewer_views.JobFileTransferView, basename='job_file_transfer'
