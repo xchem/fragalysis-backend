415ec610c5329b15b17b3501729deedab8bde07a
Kalev Takkis <ktakkis@informaticsmatters.com>
2026-01-19 12:11:22 +0000

    feat: enabled settings field in target serializer

--- viewer/serializers.py
+++ viewer/serializers.py
@@ -228,7 +228,7 @@ class TargetSerializer(serializers.ModelSerializer):
             "external_url",
             "external_url_display_name",
             "alias_order",
-            # "settings",
+            "settings",
         )
         extra_kwargs = {
             "id": {"read_only": True},
@@ -245,7 +245,7 @@ class TargetSerializer(serializers.ModelSerializer):
             "organism": {"read_only": False},
             "external_url": {"read_only": False},
             "external_url_display_name": {"read_only": False},
-            # "settings": {"read_only": False},
+            "settings": {"read_only": False},
         }
 
 
