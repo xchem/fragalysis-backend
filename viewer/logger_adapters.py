ab2df390a5a66d3ca64e4a08510809b882cea2a8
Alan B. Christie <29806285+alanbchristie@users.noreply.github.com>
2026-04-19 21:14:23 +0200

    Better TaskLoggerAdapter (#872)
    * feat: TaskLoggerAdapter now deals with optional keys
    
    * feat: Better TaskLoggerAdapter (also now accepts username)
    
    ---------
    
    Co-authored-by: a.b.christie <alan.christie@matildapeak.com>

--- viewer/logger_adapters.py
+++ viewer/logger_adapters.py
@@ -6,14 +6,25 @@ import logging
 
 class TaskLoggerAdapter(logging.LoggerAdapter):
     """
-    An adapter used by modules normally used in Celery tasks,
-    which expects a dictionary that provide 'task', 'target' and 'tas'.
+    An adapter used by modules normally used in Celery tasks. Given a dictionary that
+    provides at least a 'task' (a UUID string). It also supports 'target', 'tas', and
+    'username' keys.
+
+    The generated string always contains '.T.<task ID>' that optionally also contains
+    't(<target>)', 'tas(<tas>)', and 'u(<username>)'. If the username exists but
+    is blank, the username is replaced with '|anon|'.
     """
 
     def process(self, msg, kwargs):
         assert self.extra
-        return (
-            '/%s/ TAS(%s) Tgt(%s) %s'
-            % (self.extra['task'], self.extra['tas'], self.extra['target'], msg),
-            kwargs,
-        )
+        rendered_extra: str = f"'.T.{self.extra['task']}"
+        if 'target' in self.extra:
+            rendered_extra += f" t({self.extra['target']})"
+        if 'tas' in self.extra:
+            rendered_extra += f" tas({self.extra['tas']})"
+        if 'username' in self.extra:
+            if self.extra['username']:
+                rendered_extra += f" u({self.extra['username']})"
+            else:
+                rendered_extra += " u(|anon|)"
+        return (rendered_extra, kwargs)
