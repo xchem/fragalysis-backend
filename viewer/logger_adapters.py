2fca52092e9ac6ec64667579d9cfc585af8067df
a.b.christie <alan.christie@matildapeak.com>
2026-04-19 20:53:41 +0200

    feat: TaskLoggerAdapter now deals with optional keys

--- viewer/logger_adapters.py
+++ viewer/logger_adapters.py
@@ -6,14 +6,21 @@ import logging
 
 class TaskLoggerAdapter(logging.LoggerAdapter):
     """
-    An adapter used by modules normally used in Celery tasks,
-    which expects a dictionary that provide 'task', 'target' and 'tas'.
+    An adapter used by modules normally used in Celery tasks. Given a dictionary that
+    provides at least a 'task' (a UUID string). It also supports 'target', 'tas', and
+    'username' keys.
+
+    The generated string always contains '/<task ID>/' that optionally also contains
+    'T(<target>)', 'TAS(<tas>)', and 'U(<username>)'.
     """
 
     def process(self, msg, kwargs):
         assert self.extra
-        return (
-            '/%s/ TAS(%s) Tgt(%s) %s'
-            % (self.extra['task'], self.extra['tas'], self.extra['target'], msg),
-            kwargs,
-        )
+        rendered_extra: str = f"'/{self.extra['task']}/"
+        if 'target' in self.extra:
+            rendered_extra += f" T({self.extra['target']})"
+        if 'tas' in self.extra:
+            rendered_extra += f" TAS({self.extra['tas']})"
+        if 'username' in self.extra:
+            rendered_extra += f" U({self.extra['username']})"
+        return (rendered_extra, kwargs)
