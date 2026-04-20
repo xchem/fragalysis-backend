bfcf26d2e0bdc4537823829425a24ecf7c125006
a.b.christie <alan.christie@matildapeak.com>
2026-04-20 09:08:01 +0200

    feat: create_download_link now uses TaskLoggerAdapter

--- viewer/tasks.py
+++ viewer/tasks.py
@@ -721,7 +721,7 @@ def task_create_download_link(
     target_access_string,
 ):
     logger.info(
-        'TASK %s create_download_link launched, target_zip=%s',
+        'TASK %s launched - target_zip=%s',
         self.request.id,
         validated_data,
     )
