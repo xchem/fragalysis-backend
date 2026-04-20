31defc721debbad418feb54860d32fe52fa577ad
a.b.christie <alan.christie@matildapeak.com>
2026-04-20 11:04:33 +0200

    style: Fix task text and use first 6 characters (easier to read)

--- viewer/logger_adapters.py
+++ viewer/logger_adapters.py
@@ -10,8 +10,8 @@ class TaskLoggerAdapter(logging.LoggerAdapter):
     provides at least a 'task' (a UUID string). It also supports 'target', 'tas', and
     'username' keys.
 
-    The generated prefix string always contains the last 6 characters of the Task UUID
-    string printed as '.T.<task ID>'. The string also optionally contains
+    The generated prefix string always contains the first 6 characters of the Task UUID
+    string printed as 'T.<task ID>'. The string also optionally contains
     't(<target>)', 'tas(<tas>)', and 'u(<username>)'. If the username exists but
     is blank, the username is replaced with '|anon|'.
     """
@@ -19,9 +19,9 @@ class TaskLoggerAdapter(logging.LoggerAdapter):
     def process(self, msg, kwargs):
         assert self.extra
         if 'task' in self.extra and len(str(self.extra['task'])) > 6:
-            rendered_extra: str = f"'.T.{str(self.extra['task'])[-6:]}"
+            rendered_extra: str = f"T.{str(self.extra['task'])[:6]}"
         else:
-            rendered_extra = ".T.000000"
+            rendered_extra = "T.000000"
         if 'target' in self.extra:
             rendered_extra += f" t({self.extra['target']})"
         if 'tas' in self.extra:
